87b582f1629eda8e7a91ba5ccbea8aa74291885c
Mattias Nordqvist <mattias.nordqvist@spinit.se>
2023-10-04 13:10:20 +0200

    .

--- Results/Tap.cs
+++ Results/Tap.cs
@@ -1,6 +1,9 @@
 ﻿namespace DotNetThoughts.Results;
 public static partial class Extensions
 {
+    /// <summary>
+    /// Tap into the result and perform an action if the result is successful.
+    /// </summary>
     public static Result<T> Tap<T>(this Result<T> source, Action<T> next)
     {
         if (source.Success)
@@ -10,11 +13,38 @@ public static partial class Extensions
         return source;
     }
 
+    /// <summary>
+    /// Tap into the result and perform an action if the result is successful.
+    /// </summary>
     public static async Task<Result<T>> Tap<T>(this Task<Result<T>> source, Action<T> next)
     {
         if ((await source).Success)
         {
             next((await source).Value);
+        }
+        return await source;
+    }
+
+    /// <summary>
+    /// Tap into the result and perform an action if the result is successful.
+    /// </summary>
+    public static async Task<Result<T>> Tap<T>(this Task<Result<T>> source, Func<T, Task> next)
+    {
+        if ((await source).Success)
+        {
+            await next((await source).Value);
+        }
+        return await source;
+    }
+
+    /// <summary>
+    /// Tap into the result and perform an action if the result is successful.
+    /// </summary>
+    public static async Task<Result<T>> Tap<T>(this Result<T> source, Func<T, Task> next)
+    {
+        if (source.Success)
+        {
+            await next(source.Value);
         }
         return source;
     }
