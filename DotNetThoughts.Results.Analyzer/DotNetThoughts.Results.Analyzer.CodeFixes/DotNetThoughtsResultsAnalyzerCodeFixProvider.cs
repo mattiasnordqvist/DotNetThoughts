2444e67c6a12ab746d1d894a35db6cbc206f6b0b
Mattias Nordqvist <mattias.nordqvist@spinit.se>
2024-05-31 15:14:52 +0200

    add analyzer

---
+++ DotNetThoughts.Results.Analyzer/DotNetThoughts.Results.Analyzer.CodeFixes/DotNetThoughtsResultsAnalyzerCodeFixProvider.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+
+using System.Collections.Immutable;
+using System.Composition;
+using System.Threading.Tasks;
+
+namespace DotNetThoughts.Results.Analyzer
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(DotNetThoughtsResultsAnalyzerCodeFixProvider)), Shared]
+    public class DotNetThoughtsResultsAnalyzerCodeFixProvider : CodeFixProvider
+    {
+        public sealed override ImmutableArray<string> FixableDiagnosticIds
+        {
+            get { return ImmutableArray.Create(DotNetThoughtsResultsAnalyzerAnalyzer.DiagnosticId); }
+        }
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
