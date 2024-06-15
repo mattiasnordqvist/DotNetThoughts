f3397c28d4574fa7fb2d6bb7444766bed8699f59
Mattias Nordqvist <mattias.nordqvist@spinit.se>
2024-06-15 10:50:12 +0200

    added some conversion operators

---
+++ Results.Tests/ToNullableTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+
+using Xunit;
+
+namespace DotNetThoughts.Results.Tests;
+
+public class ToNullableTests
+{
+    [Fact]
+    public void ToNullableCompilesAndDoesNotChangeValue()
+    {
+        var a = Result<int>.Ok(123);
+        Result<int?> wishThisCouldBeCastedInstead = a.ToNullableStruct();
+        wishThisCouldBeCastedInstead.Value.Should().Be(123);
+
+        var a2 = Result<string>.Ok("123");
+        Result<string?> wishThisCouldBeCastedInstead2 = a2.ToNullable();
+        wishThisCouldBeCastedInstead2.Value.Should().Be("123");
+    }
+
+}
--- Results/Experiments.cs
+++ Results/Experiments.cs
@@ -2,7 +2,6 @@
 public static partial class Experiments
 {
     public static Func<Result<A>, Result<B>> Bind<A, B>(this Func<A, Result<B>> f) => a => a.Bind(f);
-
     public static Func<Result<A>, Result<B>> Map<A, B>(this Func<A, B> f) => a => a.Map(f);
     public static Func<Result<A>, Result<B>> Apply<A, B>(this Result<Func<A, B>> f) => a => f.Apply(a);
     public static Func<Result<A>, Result<B>> MapInTermsOfReturnAndApply<A, B>(this Func<A, B> f) => f.Return().Apply();
