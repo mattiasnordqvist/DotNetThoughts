3979eb977768d60dff35773a608d996175a5bde8
Mattias Nordqvist <mattias.nordqvist@spinit.se>
2025-03-20 10:17:58 +0100

    more bindeach bindall variants

--- Results/DotNetThoughts.Results/BindAll.cs
+++ Results/DotNetThoughts.Results/BindAll.cs
@@ -3,6 +3,8 @@ public static partial class Extensions
 {
     /// <summary>
     /// Does not short circuit. Returns all errors or one unit
+    /// 
+    /// T -> (T-> Result<Unit>) -> Result<Unit>
     /// </summary>
     public static Result<Unit> BindAll<T>(this Result<IEnumerable<T>> source, Func<T, Result<Unit>> next)
     {
@@ -12,6 +14,27 @@ public static partial class Extensions
             foreach (var s in source.Value)
             {
                 results.Add(next(s));
+            }
+            return results.Any(x => !x.Success)
+                ? UnitResult.Error(results.SelectMany(x => x.Errors))
+                : UnitResult.Ok;
+        }
+        else
+        {
+            return Result<Unit>.Error(source.Errors);
+        }
+    }
+    /// <summary>
+    /// T -> (T-> Task<Result<Unit>>) -> Task<Result<Unit>>
+    /// </summary>
+    public static async Task<Result<Unit>> BindAll<T>(this Result<IEnumerable<T>> source, Func<T, Task<Result<Unit>>> next)
+    {
+        if (source.Success)
+        {
+            var results = new List<Result<Unit>>();
+            foreach (var s in source.Value)
+            {
+                results.Add(await next(s));
             }
             return results.Any(x => !x.Success)
                 ? UnitResult.Error(results.SelectMany(x => x.Errors))
@@ -44,24 +67,7 @@ public static partial class Extensions
         }
     }
 
-    public static async Task<Result<Unit>> BindAll<T>(this Result<IEnumerable<T>> source, Func<T, Task<Result<Unit>>> next)
-    {
-        if (source.Success)
-        {
-            var results = new List<Result<Unit>>();
-            foreach (var s in source.Value)
-            {
-                results.Add(await next(s));
-            }
-            return results.Any(x => !x.Success)
-                ? UnitResult.Error(results.SelectMany(x => x.Errors))
-                : UnitResult.Ok;
-        }
-        else
-        {
-            return Result<Unit>.Error(source.Errors);
-        }
-    }
+   
 
     public static async Task<Result<Unit>> BindAll<T>(this Result<IEnumerable<T>> source, Func<T, int, Task<Result<Unit>>> next)
     {
@@ -335,6 +341,67 @@ public static partial class Extensions
             {
                 results.Add(await next(s, index));
                 index++;
+            }
+            return results.Any(x => !x.Success)
+               ? Result<List<TResult>>.Error(results.SelectMany(x => x.Errors))
+               : Result<List<TResult>>.Ok(results.Select(x => x.Value).ToList());
+        }
+        else
+        {
+            return Result<List<TResult>>.Error((await source).Errors);
+        }
+    }
+
+
+    public static async Task<Result<List<TResult>>> BindAll<T, TResult>(this Result<IReadOnlyList<T>> source, Func<T, Task<Result<TResult>>> next)
+    {
+        if (source.Success)
+        {
+            var results = new List<Result<TResult>>();
+
+            foreach (var s in source.Value)
+            {
+                results.Add(await next(s));
+            }
+            return results.Any(x => !x.Success)
+               ? Result<List<TResult>>.Error(results.SelectMany(x => x.Errors))
+               : Result<List<TResult>>.Ok(results.Select(x => x.Value).ToList());
+        }
+        else
+        {
+            return Result<List<TResult>>.Error(source.Errors);
+        }
+    }
+
+    public static async Task<Result<List<TResult>>> BindAll<T, TResult>(this Result<List<T>> source, Func<T, Task<Result<TResult>>> next)
+    {
+        if (source.Success)
+        {
+            var results = new List<Result<TResult>>();
+
+            foreach (var s in source.Value)
+            {
+                results.Add(await next(s));
+            }
+            return results.Any(x => !x.Success)
+               ? Result<List<TResult>>.Error(results.SelectMany(x => x.Errors))
+               : Result<List<TResult>>.Ok(results.Select(x => x.Value).ToList());
+        }
+        else
+        {
+            return Result<List<TResult>>.Error(source.Errors);
+        }
+    }
+
+    public static async Task<Result<List<TResult>>> BindAll<T, TResult>(this Task<Result<List<T>>> source, Func<T, Task<Result<TResult>>> next)
+    {
+        if ((await source).Success)
+        {
+            var results = new List<Result<TResult>>();
+
+            foreach (var s in (await source).Value)
+            {
+                results.Add(await next(s));
             }
             return results.Any(x => !x.Success)
                ? Result<List<TResult>>.Error(results.SelectMany(x => x.Errors))
--- Results/DotNetThoughts.Results/BindEach.cs
+++ Results/DotNetThoughts.Results/BindEach.cs
@@ -1,6 +1,7 @@
 ﻿namespace DotNetThoughts.Results;
 public static partial class Extensions
 {
+    #region input-ienumerable
     /// <summary>
     /// Short circuits on failure
     /// </summary>
@@ -62,4 +63,157 @@ public static partial class Extensions
             return Result<List<TResult>>.Error(source.Errors);
         }
     }
+
+    /// <summary>
+    /// Short circuits on failure
+    /// </summary>
+    public static async Task<Result<List<TResult>>> BindEach<T, TResult>(this Result<IEnumerable<T>> source, Func<T, Task<Result<TResult>>> next)
+    {
+        if (source.Success)
+        {
+            var result = new List<TResult>();
+            foreach (var s in source.Value)
+            {
+                var r = await next(s);
+                if (!r.Success) return Result<List<TResult>>.Error(r.Errors);
+                result.Add(r.Value);
+            }
+            return Result<List<TResult>>.Ok(result);
+        }
+        else
+        {
+            return Result<List<TResult>>.Error(source.Errors);
+        }
+    }
+
+    /// <summary>
+    /// Short circuits on failure
+    /// </summary>
+    public static async Task<Result<List<TResult>>> BindEach<T, TResult>(this Task<Result<IEnumerable<T>>> source, Func<T, Task<Result<TResult>>> next)
+    {
+        if ((await source).Success)
+        {
+            var result = new List<TResult>();
+            foreach (var s in (await source).Value)
+            {
+                var r = await next(s);
+                if (!r.Success) return Result<List<TResult>>.Error(r.Errors);
+                result.Add(r.Value);
+            }
+            return Result<List<TResult>>.Ok(result);
+        }
+        else
+        {
+            return Result<List<TResult>>.Error((await source).Errors);
+        }
+    }
+    #endregion
+
+    #region input-list
+    /// <summary>
+    /// Short circuits on failure
+    /// </summary>
+    public static Result<Unit> BindEach<T>(this Result<List<T>> source, Func<T, Result<Unit>> next)
+    {
+        if (source.Success)
+        {
+            foreach (var s in source.Value)
+            {
+                var result = next(s);
+                if (!result.Success) return result;
+            }
+            return UnitResult.Ok;
+        }
+        else
+        {
+            return UnitResult.Error(source.Errors);
+        }
+    }
+
+    /// <summary>
+    /// Short circuits on failure
+    /// </summary>
+    public static async Task<Result<Unit>> BindEach<T>(this Result<List<T>> source, Func<T, Task<Result<Unit>>> next)
+    {
+        if (source.Success)
+        {
+            foreach (var s in source.Value)
+            {
+                var result = await next(s);
+                if (!result.Success) return result;
+            }
+            return UnitResult.Ok;
+        }
+        else
+        {
+            return UnitResult.Error(source.Errors);
+        }
+    }
+
+    /// <summary>
+    /// Short circuits on failure
+    /// </summary>
+    public static Result<List<TResult>> BindEach<T, TResult>(this Result<List<T>> source, Func<T, Result<TResult>> next)
+    {
+        if (source.Success)
+        {
+            var result = new List<TResult>();
+            foreach (var s in source.Value)
+            {
+                var r = next(s);
+                if (!r.Success) return Result<List<TResult>>.Error(r.Errors);
+                result.Add(r.Value);
+            }
+            return Result<List<TResult>>.Ok(result);
+        }
+        else
+        {
+            return Result<List<TResult>>.Error(source.Errors);
+        }
+    }
+
+    /// <summary>
+    /// Short circuits on failure
+    /// </summary>
+    public static async Task<Result<List<TResult>>> BindEach<T, TResult>(this Result<List<T>> source, Func<T, Task<Result<TResult>>> next)
+    {
+        if (source.Success)
+        {
+            var result = new List<TResult>();
+            foreach (var s in source.Value)
+            {
+                var r = await next(s);
+                if (!r.Success) return Result<List<TResult>>.Error(r.Errors);
+                result.Add(r.Value);
+            }
+            return Result<List<TResult>>.Ok(result);
+        }
+        else
+        {
+            return Result<List<TResult>>.Error(source.Errors);
+        }
+    }
+
+    /// <summary>
+    /// Short circuits on failure
+    /// </summary>
+    public static async Task<Result<List<TResult>>> BindEach<T, TResult>(this Task<Result<List<T>>> source, Func<T, Task<Result<TResult>>> next)
+    {
+        if ((await source).Success)
+        {
+            var result = new List<TResult>();
+            foreach (var s in (await source).Value)
+            {
+                var r = await next(s);
+                if (!r.Success) return Result<List<TResult>>.Error(r.Errors);
+                result.Add(r.Value);
+            }
+            return Result<List<TResult>>.Ok(result);
+        }
+        else
+        {
+            return Result<List<TResult>>.Error((await source).Errors);
+        }
+    }
+    #endregion
 }
